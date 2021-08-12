8ba9b004b3deb5fa96e0db4fe89292dc5307f9cc
wisardstat <wisardstat@gmail.com>
2021-08-12 07:55:46 +0700

    Upload ไฟล์เพิ่มเติม

---
+++ dbContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using APIapplication.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace APIapplication
+{
+
+        public class dbContext : DbContext
+        {
+            public dbContext(DbContextOptions<dbContext> options)
+                : base(options)
+            {
+            }
+            public DbSet<tbl_role> tbl_role { get; set; }
+            public DbSet<TbStockCard> TbStockCard { get; set; }
+            public DbSet<tbWareHouse> tbWareHouse { get; set; }
+
+
+    }
+    
+}
